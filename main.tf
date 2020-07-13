c52ae4f67531570b17d5907a878469dfed5ff49b
Stuart Izon <stuart.izon@gmail.com>
2020-07-13 22:33:08 +1000

    Remove VPC and add a common ECS cluster

--- main.tf
+++ main.tf
@@ -13,6 +13,9 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_ecs_cluster" "default" {
+  name = "default"
+  capacity_providers = [
+    "FARGATE"
+  ]
 }
